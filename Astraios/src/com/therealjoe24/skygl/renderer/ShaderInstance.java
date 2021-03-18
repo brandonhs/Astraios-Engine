83e0231a8dae8b2fd6b2dd3af4756ca60b1d6eb0
Brandon S <brandonnms@gmail.com>
2021-03-17 19:12:20 -0700

    Update license header text

--- Astraios/src/com/therealjoe24/skygl/renderer/ShaderInstance.java
+++ Astraios/src/com/therealjoe24/skygl/renderer/ShaderInstance.java
@@ -1,7 +1,8 @@
 /*******************************************************************************
+ * This file is part of the Astraios Engine distribution <https://github.com/TheRealJoe24/Astraios-Engine>
  * Copyright (C) 2021 TheRealJoe24
  * 
- * This program is free software: you can redistribute it and/or modify
+ * Astraios Engine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
